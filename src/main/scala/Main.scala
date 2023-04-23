745b651debf6d8b7bbd3af2ca8de78b731c5cf15
Davide Cassatella <davide.cassatella@accenture.com>
2023-04-23 21:23:54 +0200

    Added some functions -> max, min, sum, dif

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,9 +12,34 @@ object ScalaBook {
 
     var capital = Map("Italia" -> "Roma", "Germania" -> "Berlino")
     capital += ("Giappone" -> "Tokio")
-    println(capital("Italia"))
+    //println(capital("Italia"))
   }
 
+  // DEFINE SOME FUNCTIONS
+  // - def -> Definire una funzione
+  // - Nome della funzione in minuscolo
+  // - Lista di parametri nella (parentesi)
+  // - Type risultante dalla funzione :
+  // - Body della funzione in {parentesi graffe}
+
+  def max(x: Int, y: Int): Int = {
+    if (x >= y) x
+    else y
+  }
+  // Call max
+  println("The max value between "+ 5 + " and " + 9 + " is " + max(5,9))
+  def min(x: Int, y: Int): Int = if (x <= y) x else y
+  // Call min
+  println("The min value between "+ 8 + " and " + 3 + " is " + min(8,3))
+
+  def sum(x: Int, y: Int) = x + y
+  // Call sum
+  println("The sum of "+ 2 + " and " + 5 + " is " +sum(2,5))
+
+  def dif (x: Int, y: Int): Int = {
+    x - y
+  }
+  println("The difference between "+ 9 + " and " + 5 + " is " + dif(9,5))
 
 
 }
